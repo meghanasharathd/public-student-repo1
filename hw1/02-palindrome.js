c3afb162bf4e41384ec7859ebafead682367e191
miles <miles4599@gmail.com>
2023-04-09 16:54:28 -0700

    checks that user enters positive number palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,33 @@
-const elem = document.querySelector('input');
+function isPositiveNumber(str) {
+  if ((!Number.isNaN(str) && Number(str) > 0)) {
+    return true;
+  }
+  return false;
+}
 
-elem.addEventListener('input', handleInput);
+function isPalindrome(str) {
+  const len = str.length;
+  const halfway = Math.floor(len / 2);
+  for (let i = 0; i < halfway; i += 1) {
+    if (!(str[i] === str[len - i - 1])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const inputBox = document.querySelector('input');
+const result = document.getElementById('result');
+
+function handleEvent() {
+  if (isPositiveNumber(inputBox.value) && isPalindrome(inputBox.value)) {
+    result.innerHTML = 'Yes. This is a palindrome!';
+  } else {
+    result.innerHTML = 'No. Try again';
+  }
+}
+
+inputBox.addEventListener(
+  'input',
+  handleEvent,
+);
