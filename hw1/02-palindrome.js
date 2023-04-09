1b481c9c87a5f9c00d69aab4d3fa5eae1096cf31
Cristian Torres Salazar <tor8@pdx.edu>
2023-04-09 02:00:14 -0700

    Javascript for exercise two "Palendrome" complete

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,31 @@
-const elem = document.querySelector('input');
+const input = document.querySelector('input');
+const isPalendromeTag = document.querySelector('#IsPalendrome');
+const isOnlyNumbersExp = new RegExp(/^(-*([0-9]+.?)[0-9]*)$/);
 
-elem.addEventListener('input', handleInput);
+const handleInput = () => {
+  if (isOnlyNumbersExp.test(input.value.toString())) {
+    if (input.value >= 0) {
+      let ogString = input.value.toString();
+      let revString = input.value.toString().split('').reverse().join('');
+
+      if (ogString === revString) {
+        isPalendromeTag.textContent = 'Yes, This is a Palendrome.';
+        isPalendromeTag.style.color = 'darkgreen';
+      } else {
+        isPalendromeTag.textContent = 'Not a Palendrome. Try Again.';
+        isPalendromeTag.style.color = 'darkred';
+      }
+    } else if (input.value < 0) {
+      isPalendromeTag.textContent = 'Number must be greater than 0. Try Again.';
+      isPalendromeTag.style.color = 'darkred';
+    } else {
+      isPalendromeTag.textContent = 'Error Processing Input';
+      isPalendromeTag.style.color = 'darkred';
+    }
+  } else {
+    isPalendromeTag.textContent = 'Input Must Be Numbers. Try Again';
+    isPalendromeTag.style.color = 'darkred';
+  }
+};
+
+input.addEventListener('input', handleInput);
