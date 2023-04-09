c8c40579daf9c27a267c363329b234cf0d9d10c1
devonfox <devonhouston.fox@gmail.com>
2023-04-09 14:26:02 -0700

    first pass palindrome function

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,30 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  let input = event.target.value;
+  if (isPalindrome(input)) {
+    console.log(input + ' is a palindrome.');
+  }
+}
+
+function isPalindrome(input) {
+  let inputCheck = parseInt(input);
+  if (isNaN(inputCheck)) {
+    console.log('Not a number.');
+    return false;
+  } else if (inputCheck < 0) {
+    console.log('Not a positive number.');
+    return false;
+  }
+  let reversedInput = '';
+  for (i = input.length - 1; i >= 0; i--) {
+    reversedInput += input[i];
+  }
+  if (input.toString() === reversedInput) {
+    return true;
+  }
+  console.log('Not a palindrome.');
+  return false;
+}
