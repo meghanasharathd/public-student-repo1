f73397e9da58796661bb5a07b0db0a19615f7e1e
miles <miles4599@gmail.com>
2023-04-10 15:29:54 -0700

    Added some comments

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,11 +1,26 @@
-function isPositiveNumber(str) {
+/**
+ * Checks if a string is a positive number or not.
+ *
+ * Examples:
+ *
+ * '0'    : false \
+ * 'abc'  : false \
+ * '-1'   : false \
+ * '001'  : true \
+ * '1.23' : true \
+ * 'Infinity' : true
+ */
+const isPos = function isPositiveNumber(str) {
   if (!Number.isNaN(str) && Number(str) > 0) {
     return true;
   }
   return false;
-}
+};
 
-function isPalindrome(str) {
+/**
+ * Checks if a string is a palindrome or not.
+ */
+const isPal = function isPalindrome(str) {
   const len = str.length;
   const halfway = Math.floor(len / 2);
   for (let i = 0; i < halfway; i += 1) {
@@ -14,22 +29,25 @@ function isPalindrome(str) {
     }
   }
   return true;
-}
+};
 
 const inputBox = document.querySelector('input');
 const result = document.getElementById('result');
 
-function handleEvent() {
-  if (!isPositiveNumber(inputBox.value)) {
+/**
+ * Display correct message in response to any user input into `inputBox`.
+ */
+const handleEvent = function handleEvent() {
+  if (!isPos(inputBox.value)) {
     result.style.setProperty('color', 'darkred');
     result.innerHTML = '<b>Input must be a positive number.</b>';
-  } else if (!isPalindrome(inputBox.value)) {
+  } else if (!isPal(inputBox.value)) {
     result.style.setProperty('color', 'darkred');
     result.innerHTML = '<b>No. Try again</b>';
   } else {
     result.style.setProperty('color', 'darkgreen');
     result.innerHTML = '<b>Yes. This is a palindrome!</b>';
   }
-}
+};
 
 inputBox.addEventListener('input', handleEvent);
