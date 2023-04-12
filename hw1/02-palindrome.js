a304b6be10c231edf313250cbf96f497d80b6874
Rajashree Pethkar <rpethkar@pdx.edu>
2023-04-12 11:30:37 -0700

    Resolved some code review comments

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,15 +1,15 @@
-const elem = document.querySelector("input");
+const elem = document.querySelector('input');
 
-elem.addEventListener("input", handleInput);
+elem.addEventListener('input', handleInput);
 
-let output = document.getElementById("output");
-const newP = document.createElement("p");
+const output = document.getElementById('output');
+const newP = document.createElement('p');
 output.appendChild(newP);
 
 function handleInput(event) {
-  newP.textContent = " ";
+  newP.textContent = ' ';
   let inputNumber = parseInt(event.target.value);
-  if (!isNaN(inputNumber)) {
+  if (!Number.isNaN(inputNumber)) {
     if (inputNumber >= 0) {
       const temp = inputNumber;
       let reversed = 0;
@@ -19,7 +19,7 @@ function handleInput(event) {
         inputNumber = parseInt(inputNumber / 10);
       }
 
-      if (reversed == temp) {
+      if (reversed === temp) {
         newP.style.color = "green";
         newP.textContent = "Yes. This is a palindrome";
       } else {
@@ -27,10 +27,10 @@ function handleInput(event) {
         newP.textContent = "No. Try again.";
       }
     } else {
-        newP.style.color = "red";
-        newP.textContent = "No. Try again.";
+      newP.style.color = "red";
+      newP.textContent = "No. Try again.";
     }
-  }else{
+  } else {
     newP.textContent = " ";
   }
 }
