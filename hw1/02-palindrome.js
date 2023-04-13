10b93d3fa58af9392f38ca5db58ea991909a82ba
Cristian Torres Salazar <tor8@pdx.edu>
2023-04-12 18:19:22 -0700

    Fixed Spelling of "Palindrome"

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,14 +1,14 @@
 const input = document.querySelector('input');
-const isPalendromeTag = document.querySelector('#IsPalendrome');
+const isPalindromeTag = document.querySelector('#IsPalindrome');
 // regular expression: passes neg/pos integers and decimals.
 const isOnlyNumbersExp = new RegExp(/^(-*([0-9]+.?)[0-9]*)$/);
 
 //  Method: OnUserInput
 // Purpose: This method uses a regular expression to confirm the user has entered correct input
-//          and if so will determine if the input is a Palendrome while also handling
+//          and if so will determine if the input is a Palindrome while also handling
 //          various edge cases.
 // Remarks:
-//      - I've allowed decimal values since 12.21 is a palendrome.
+//      - I've allowed decimal values since 12.21 is a Palindrome.
 //      - Text color for error messages were changed to "darkred" because "red" triggerd a WAVE accessibility error.
 //      - Negative values will pass the regular expression but will be handled within the methods logic.
 const OnUserInput = () => {
@@ -22,25 +22,25 @@ const OnUserInput = () => {
       let revString = input.value.toString().split('').reverse().join('');
 
       if (ogString === revString) {
-        isPalendromeTag.textContent = 'Yes, This is a Palendrome.';
-        isPalendromeTag.style.color = 'green';
+        isPalindromeTag.textContent = 'Yes, This is a Palindrome.';
+        isPalindromeTag.style.color = 'green';
       } else {
-        isPalendromeTag.textContent = 'Not a Palendrome. Try Again.';
-        isPalendromeTag.style.color = 'darkred';
+        isPalindromeTag.textContent = 'Not a Palindrome. Try Again.';
+        isPalindromeTag.style.color = 'darkred';
       }
       // handle negative values
     } else if (input.value < 0) {
-      isPalendromeTag.textContent = 'Number must be greater than 0. Try Again.';
-      isPalendromeTag.style.color = 'darkred';
+      isPalindromeTag.textContent = 'Number must be greater than 0. Try Again.';
+      isPalindromeTag.style.color = 'darkred';
       // handle unforseen errors
     } else {
-      isPalendromeTag.textContent = 'Error Processing Input';
-      isPalendromeTag.style.color = 'darkred';
+      isPalindromeTag.textContent = 'Error Processing Input';
+      isPalindromeTag.style.color = 'darkred';
     }
     // handle non numerics,
   } else {
-    isPalendromeTag.textContent = 'Input Must Be Numbers. Try Again';
-    isPalendromeTag.style.color = 'darkred';
+    isPalindromeTag.textContent = 'Input Must Be Numbers. Try Again';
+    isPalindromeTag.style.color = 'darkred';
   }
 };
 
