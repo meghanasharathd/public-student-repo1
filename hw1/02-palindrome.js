a2d1f47eaf9b194b39e97e2bd7262df8580b8f4f
Corey <cpatri2@pdx.edu>
2023-04-15 11:55:08 -0700

    Added an id to the input tag and a for for the associated label for accessibility. Also added a check to make sure the value input is non-zero in the palindrome check.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -3,7 +3,7 @@ const isPalindrome = document.getElementById('isPalindrome');
 const notPalindrome = document.getElementById('notPalindrome');
 
 const handleInput = function handleUserInput() {
-  if (input.value >= 0) {
+  if (input.value >= 0 && input.value.length > 0) {
     const str = input.value.toString();
     const reversedString = str.split('').reverse().join('');
     if (str === reversedString) {
