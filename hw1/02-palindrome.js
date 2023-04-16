ce74ffac03a103110690a10c69ec5442d0a4efc2
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-04-16 00:42:39 -0700

    Quotes updated
    Quotes updated to single quotes

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,28 +1,28 @@
-const elem = document.querySelector("input");
+const elem = document.querySelector('input');
 
-elem.addEventListener("input", handleInput);
+elem.addEventListener('input', handleInput);
 
-let result = document.getElementById("result");
-let answer = document.createElement("p");
-answer.setAttribute("class", "output");
+let result = document.getElementById('result');
+let answer = document.createElement('p');
+answer.setAttribute('class', 'output');
 result.append(answer);
 
 function handleInput(event) {
   var value = event.target.value;
   //hint found from: https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
-  var valueReversed = value.split("").reverse().join("");
+  var valueReversed = value.split('').reverse().join('');
 
   if (isNaN(value)) {
-    answer.textContent = "Enter a number";
-    answer.style.color = "red";
+    answer.textContent = 'Enter a number';
+    answer.style.color = 'red';
   } else if (value < 0) {
-    answer.textContent = "Enter a positive number";
-    answer.style.color = "red";
+    answer.textContent = 'Enter a positive number';
+    answer.style.color = 'red';
   } else if (value === valueReversed) {
-    answer.textContent = "Yes. This is a palindrome!";
-    answer.style.color = "green";
+    answer.textContent = 'Yes. This is a palindrome!';
+    answer.style.color = 'green';
   } else {
-    answer.textContent = "No, Try again";
-    answer.style.color = "red";
+    answer.textContent = 'No, Try again';
+    answer.style.color = 'red';
   }
 }
