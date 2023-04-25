58e36a9c3b7124d19e4b0798f9bd107416340fd2
Cristian Torres Salazar <tor8@pdx.edu>
2023-04-24 21:20:26 -0700

    Completes Exercise 3 - Count

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,23 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+const text = document.querySelector('#TextContent').innerText;
+let keys = '';
+
+function handleKeyDown(event) {
+  if (event.key === 'Shift') return;
+
+  let keyWord = document.querySelector('input').value;
+
+  if (keyWord === '') {
+    keyWord = event.key;
+  }
+  let textCopy = text;
+  textCopy = textCopy.replaceAll(
+    keyWord,
+    `<span class="highlight">${keyWord}</span>`
+  );
+
+  document.querySelector('#TextContent').innerHTML = textCopy;
+}
+
+document.getElementById('UserInput').addEventListener('keydown', handleKeyDown);
