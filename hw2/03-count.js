b0e10bfe0de1a386f0a50bd769943c419b6b82b8
CristianTS599 <cristiant.salazar@gmail.com>
2023-04-25 07:27:44 -0700

    Commenting and Completed WAVE Audit for 03-Count

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,8 +1,16 @@
 // Add your code here
 
+// I want a non changing copy of the paragraph
 const text = document.querySelector('#TextContent').innerText;
-let keys = '';
 
+//  Method: handleKeyDown
+// Purpose: Will trigger the highlighting functionality on the paragragh by creating a copy 
+//          of the text and replace all keywords given by the user by a span with a higlight class.
+//          then this modified text is added as the inner html.
+//    Args: event is the keyDown event object
+// Remarks: Need to come backt to this one and grab the target element from the
+//          from the event rather than from the dom and then I need to eliminate some noise
+//          happening with empty events.
 function handleKeyDown(event) {
   if (event.key === 'Shift') return;
 
