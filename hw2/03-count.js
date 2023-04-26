28780c1fe5949bbfe995413a0d00a5354e6a35fe
Corey <cpatri2@pdx.edu>
2023-04-26 09:58:07 -0700

    Finished part 3 of the homework

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,19 @@
 // Add your code here
 
+const input = document.getElementById('userInput');
+const textField = document.getElementById('text');
+const highlightedText = document.getElementById('highlightedText');
+
+const handleKeyDown = function handleUserKeyDown(event) {
+  if (event.keyCode === 13) {
+    const searchTerm = input.value.trim();
+    const searchRegex = new RegExp(`\\b${searchTerm}\\b`, 'gi');
+    const searchContent = textField.innerHTML;
+    const highlightedContent = searchContent.replace(searchRegex, '<span style="background-color: yellow;">$&</span>');
+    highlightedText.innerHTML = highlightedContent;
+    textField.style.display = 'none';
+    highlightedText.style.display = 'block';
+  }
+};
+
 input.addEventListener('keydown', handleKeyDown);
