638617662af7c3ea88d8e2b209b5e7d4eb9c26ba
Corey <cpatri2@pdx.edu>
2023-04-26 13:25:22 -0700

    Finished part 4

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,32 @@
 // Add your code here
+const body = document.getElementById('background');
+const timeInterval = document.getElementById('interval');
+const changeColorBtn = document.getElementById('changeColorBtn');
+
+const colorChange = function randomColorChangeForBodyBackground() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const a = Math.floor(Math.random() * 10) / 10;
+  body.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${a})`;
+};
+let colorChangeInterval = setInterval(colorChange, 3000);
+
+const handleIntervalChange = function handleUserIntervalChange() {
+  if (changeColorBtn.innerText === 'Start') {
+    if (timeInterval.value > 0) {
+      clearInterval(colorChangeInterval);
+      colorChangeInterval = setInterval(colorChange, timeInterval.value * 1000);
+      changeColorBtn.innerText = 'Stop';
+      changeColorBtn.classList.remove('btn-primary');
+      changeColorBtn.classList.add('btn-danger');
+    }
+  } else {
+    clearInterval(colorChangeInterval);
+    changeColorBtn.innerText = 'Start';
+    changeColorBtn.classList.remove('btn-danger');
+    changeColorBtn.classList.add('btn-primary');
+  }
+};
+window.onload = colorChangeInterval;
+changeColorBtn.addEventListener('click', handleIntervalChange);
