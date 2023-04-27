7f08b1d9ac1702a85211c27ddf2384d7e85c3395
Corey <cpatri2@pdx.edu>
2023-04-26 19:45:23 -0700

    Changed keydown event to keyup to check for typed word when user is finished typing instead of when enter is pressed, like Shreya suggested.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -4,16 +4,14 @@ const input = document.getElementById('userInput');
 const textField = document.getElementById('text');
 const highlightedText = document.getElementById('highlightedText');
 
-const handleKeyDown = function handleUserKeyDown(event) {
-  if (event.keyCode === 13) {
-    const searchTerm = input.value.trim();
-    const searchRegex = new RegExp(`\\b${searchTerm}\\b`, 'gi');
-    const searchContent = textField.innerHTML;
-    const highlightedContent = searchContent.replace(searchRegex, '<span style="background-color: yellow;">$&</span>');
-    highlightedText.innerHTML = highlightedContent;
-    textField.style.display = 'none';
-    highlightedText.style.display = 'block';
-  }
+const handleKeyDown = function handleUserKeyDown() {
+  const searchTerm = input.value.trim();
+  const searchRegex = new RegExp(`\\b${searchTerm}\\b`, 'gi');
+  const searchContent = textField.innerHTML;
+  const highlightedContent = searchContent.replace(searchRegex, '<span style="background-color: yellow;">$&</span>');
+  highlightedText.innerHTML = highlightedContent;
+  textField.style.display = 'none';
+  highlightedText.style.display = 'block';
 };
 
-input.addEventListener('keydown', handleKeyDown);
+input.addEventListener('keyup', handleKeyDown);
