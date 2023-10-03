1d03a591e84dbf9a4d8283e212b6107d7dde9af8
SayleeMJ <sjagtap@pdx.edu>
2023-10-03 10:36:42 -0700

    Completed 02-Palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,33 @@
-const elem = document.querySelector('input');
+function isPalindrome(value) {
+  const stringInput = value.toString();
+  const reversedInput = stringInput.split("").reverse().join("");
+  return stringInput === reversedInput;
+}
 
-elem.addEventListener('input', handleInput);
+const displayPalindromeResult = (resultColorClass, resultTextString) => {
+  const displayResult = document.getElementById("result");
+  displayResult.className = resultColorClass;
+  displayResult.textContent = resultTextString;
+};
+const elem = document.querySelector("input");
+
+const handleInput = () => {
+  const input = parseInt(elem.value);
+
+  if (!Number.isNaN(input)) {
+    if (input >= 0) {
+      const isInputPalindrome = isPalindrome(input);
+      if (isInputPalindrome) {
+        displayPalindromeResult("text-success", "Yes. This is a palindrome!");
+      } else {
+        displayPalindromeResult("text-danger", "No. Try again.");
+      }
+    } else {
+      displayPalindromeResult("text-danger", "Enter a positive number!");
+    }
+  } else {
+    displayPalindromeResult("text-black", "Please enter a number first!");
+  }
+};
+
+elem.addEventListener("input", handleInput);
