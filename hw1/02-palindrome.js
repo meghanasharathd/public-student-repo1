d8d7976f2dacabd9214521001efd3bf8134ecfe5
meghanasharathd <146280196+meghanasharathd@users.noreply.github.com>
2024-04-17 12:33:18 -0700

    Commits for HW1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,33 @@
 const elem = document.querySelector('input');
-
+const res = document.getElementById('result');
+;
 elem.addEventListener('input', handleInput);
+
+function handleInput(){
+
+      
+        const val = document.getElementById('num').value;
+        res.textContent = "";
+        if(val === ""){
+            return;
+        }
+        if(val < 0){
+            return "Not a positive number";
+        }
+
+        else{
+        const str = val.toString();
+    
+        const reversedStr = str.split('').reverse().join('');
+    
+        
+        if (str === reversedStr) {
+           res.textContent = "Yes, it is a palindrome!";
+           res.style.color = "green";
+        } else {
+           res.textContent = "No, Try again!";
+           res.style.color = "red";
+        }
+
+    }
+}
