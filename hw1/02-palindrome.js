95d7984e8dc6328ce9137fdaa4edda194c1d08f1
meghanasharathd <146280196+meghanasharathd@users.noreply.github.com>
2024-04-18 14:56:42 -0700

    HW1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,34 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+const res = document.getElementById('result');
+
+
+function handleInput(){
+
+
+        const val = document.getElementById('num').value;
+        res.textContent = "";
+        if(val === ""){
+            return;
+        }
+        if(val < 0){
+            return "Not a positive number";
+        }
+
+        else{
+        const str = val.toString();
+
+        const reversedStr = str.split('').reverse().join('');
+
+
+        if (str === reversedStr) {
+           res.textContent = "Yes, it is a palindrome!";
+           res.style.color = "green";
+        } else {
+           res.textContent = "No, Try again!";
+           res.style.color = "red";
+        }
+
+    }
+} 
